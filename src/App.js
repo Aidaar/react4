86c938b4cf6183535657b2e5f4f930966e3629e3
Aidaar <aidar21mamatov@gmail.com>
2024-04-04 17:15:52 +0600

    add function on add tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+const TodoList = () => {
+	const [tasks, setTasks] = useState([]);
+	const [editingTask, setEditingTask] = useState(null)
+	const [newTask, setNewTask] = useState("");
+
+	const handleAddTask = () => {
+		if (newTask) {
+			setTasks([...tasks, newTask])
+			setNewTask("")
+		}
+	}
+
+
+
+	return (
+		<div>
+			<h1>To Do List</h1>
+			<ul>
+				{
+					tasks.map((task, index) => (
+						<li key={index}>
+							{editingTask === index ? (
+								<input type='text' value={newTask} onChange={handleInputChange}/>
+							) : (
+								task
+							)}
+							{
+								editingTask === index ? (
+									<button onClick={handleSaveTask}>Save Task</button>
+								) : (
+									<button onClick={() => handleEditTask(index)}>Change</button>
+								)
+							}
+						</li>
+
+					))
+				}
+			</ul>
+			<input type="text" value={newTask} onChange={handleInputChange}/>
+			<button onClick={handleAddTask}>Add</button>
+		</div>
+	)
+}
+
 
 
 function App() {
-  return (
-			<div className="App">
-			
-			</div>
-  );
+	return <TodoList />
 }
 
-export default App;
+export default App
